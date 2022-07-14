f0167d03d7b06936fde47e9821e5257d126d438c
delanobmarques <delanobmarques@gmail.com>
2022-07-14 17:59:04 -0300

    update favicon and add react-unicons to the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import UilReact from '@iconscout/react-unicons/icons/uil-react';
+
 const  App = () => {
   return (
     <div className="App">
-      My app
+      <h1 className="text-2xl font-bold text-red-500">Delano</h1>
+      <UilReact size={100}/>      
     </div>
   );
 }
